3d60394db2a3adc6db9eacf08f657b33038fe026
John Ross <jlross42@gmail.com>
2024-10-17 21:17:13 -0400

    enable typical db connections

--- once_human/database.py
+++ once_human/database.py
@@ -1,36 +1,47 @@
 from contextlib import contextmanager
-
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from once_human.config import config, GoogleSheetsDatabaseSettings
+from once_human.config import (
+    config,
+    GoogleSheetsDatabaseSettings,
+    GenericDatabaseSettings,
+)
+from once_human.models import Base
 
 
-def get_mysql_engine():
-    raise NotImplementedError
+def create_generic_database():
+    db_settings: GenericDatabaseSettings = config.db
+    dialect = db_settings.dialect
+    if db_settings.driver != "default":
+        dialect = f"{dialect}+{db_settings.driver}"
+    connection_string = f"{dialect}://{db_settings.user}:{db_settings.password}@{db_settings.host}/{db_settings.name}"
+    engine = create_engine(connection_string, echo=True)
+    Base.metadata.create_all(bind=engine)
+    session_local_class = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = session_local_class()
+    return db
 
 
-def get_google_sheets_engine():
+def create_gsheets_database():
     db_settings: GoogleSheetsDatabaseSettings = config.db
     engine = create_engine(
         "gsheets://",
         service_account_info=db_settings.service_account,
         catalog=db_settings.catalog(),
     )
-    return engine
+    session_local_class = sessionmaker(bind=engine)
+    db = session_local_class()
+    return db
 
 
 @contextmanager
 def get_db():
     if config.db.dialect == "gsheets":
-        engine = get_google_sheets_engine()
-    elif config.db.dialect == "mysql":
-        engine = get_mysql_engine()
+        db = create_gsheets_database()
     else:
-        raise ValueError(f"unknown db dialect: {config.db.dialect}")
+        db = create_generic_database()
 
-    SessionLocal = sessionmaker(bind=engine)
-    db = SessionLocal()
     try:
         yield db
     finally:
