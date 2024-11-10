08c1373444cfe94d6c2626e57d9fb2ab85bea59e
John Ross <jlross42@gmail.com>
2024-11-10 17:25:00 -0500

    add constant

--- once_human/bot/utils.py
+++ once_human/bot/utils.py
@@ -9,6 +9,8 @@ from once_human.models import Base
 type InteractionCallback[T] = Callable[[T, ..., discord.Interaction], Awaitable[None]]
 type DatabaseModel[T: Base, R] = Callable[[T], R]
 
+ZERO_WIDTH_SPACE = "\u200b"
+
 
 def response(interaction: discord.Interaction) -> InteractionResponse:
     # just a way to avoid that bad type hinting of discord.py
