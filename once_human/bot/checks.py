496a62fcdc81600ae85e4b6f50799f986898130c
John Ross <jlross42@gmail.com>
2024-11-10 00:22:18 -0500

    add checks

--- once_human/bot/checks.py
+++ once_human/bot/checks.py
@@ -4,20 +4,20 @@ from once_human.config import config
 
 
 def is_admin():
-    def predicate(interaction: discord.Interaction) -> bool:
+    def is_guild_owner(interaction: discord.Interaction) -> bool:
         return interaction.user.id == interaction.guild.owner_id
 
     if config.discord.admin_role:
         return app_commands.checks.has_role(config.discord.admin_role)
     else:
-        return app_commands.check(predicate)
+        return app_commands.check(is_guild_owner)
 
 
 def is_user():
-    def predicate(interaction: discord.Interaction) -> bool:
+    def always_true(interaction: discord.Interaction) -> bool:
         return True
 
     if config.discord.user_role:
         return app_commands.checks.has_role(config.discord.user_role)
     else:
-        return app_commands.check(predicate)
+        return app_commands.check(always_true)
