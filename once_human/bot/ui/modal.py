9e79aac87e8adf66c0f4a3eee3a3ec9a8e395a57
John Ross <jlross42@gmail.com>
2024-11-14 03:11:23 -0500

    changes to modal

--- once_human/bot/ui/modal.py
+++ once_human/bot/ui/modal.py
@@ -1,14 +1,21 @@
 import discord.ui
+from discord import Interaction
 
-from once_human.bot.utils import InteractionCallback
+from once_human.bot.ui.views.base import BaseView
+from once_human.bot.utils import response
 
 
 class BaseModal(discord.ui.Modal):
-    def __init__(
-        self,
-        *,
-        on_submit: InteractionCallback,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.on_submit = on_submit
+    def __init__(self, parent_view: BaseView, *, title: str, text_inputs: list[discord.ui.TextInput]):
+        super().__init__(title=title)
+        self.parent_view = parent_view
+        for item in text_inputs:
+            self.add_item(item)
+
+    async def on_submit(self, interaction: Interaction) -> None:
+        self.parent_view.interaction = interaction
+        self.stop()
+
+    async def show(self) -> None:
+        await response(self.parent_view.interaction).send_modal(self)
+        await self.wait()
